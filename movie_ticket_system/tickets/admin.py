5bbd72222be3de28c8f6855716f6e0b2cd468299
Nush-Faiz <nushkiyafaiz15@gmail.com>
2025-04-20 13:07:38 +0530

    Add coming soon movies, auto-update to now showing, improve admin panel, and include release date

--- movie_ticket_system/tickets/admin.py
+++ movie_ticket_system/tickets/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from .models import Movie, Theater, Showtime, Booking
 
-admin.site.register(Movie)
-admin.site.register(Theater)
-admin.site.register(Showtime)
-admin.site.register(Booking)
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_released', 'release_date', 'genre', 'rating')
+    list_filter = ('is_released', 'genre', 'rating')
+    search_fields = ('title', 'description')
+    list_editable = ('is_released',)
+
+@admin.register(Theater)
+class TheaterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'capacity')
+
+@admin.register(Showtime)
+class ShowtimeAdmin(admin.ModelAdmin):
+    list_display = ('movie', 'theater', 'start_time', 'available_seats')
+    list_filter = ('movie', 'theater')
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'showtime', 'seats', 'booked_at')
+    list_filter = ('showtime',)
--- movie_ticket_system/tickets/views.py
+++ movie_ticket_system/tickets/views.py
@@ -8,28 +8,37 @@ def home(request):
     format = request.GET.get('format')
     language = request.GET.get('language')
 
+    now_showing = Movie.objects.filter(is_released=True)
+    upcoming = Movie.objects.filter(is_released=False).order_by('release_date')
 
-    movies = Movie.objects.all()
 
     if search_query:
-        movies = movies.filter(title__icontains=search_query)
+        now_showing = now_showing.filter(title__icontains=search_query)
+        upcoming = upcoming.filter(title__icontains=search_query)
 
     if genre and genre != "All":
-        movies = movies.filter(genre=genre)
+        now_showing = now_showing.filter(genre=genre)
+        upcoming = upcoming.filter(genre=genre)
+
     if format and format != "All":
-        movies = movies.filter(format=format)
+        now_showing = now_showing.filter(format=format)
+        upcoming = upcoming.filter(format=format)
+
     if language and language != "All":
-        movies = movies.filter(language=language)
+        now_showing = now_showing.filter(language=language)
+        upcoming = upcoming.filter(language=language)
+
 
     context = {
-        'movies': movies,
+        'now_showing': now_showing,
+        'upcoming': upcoming,
         'search_query': search_query,
         'selected_genre': genre or "All",
         'selected_format': format or "All",
         'selected_language': language or "All",
     }
 
-    return render(request, 'tickets/home.html', {'movies': movies})
+    return render(request, 'tickets/home.html', {'now_showing': now_showing, 'upcoming': upcoming })
 
 
 def movie_detail(request, movie_id):
