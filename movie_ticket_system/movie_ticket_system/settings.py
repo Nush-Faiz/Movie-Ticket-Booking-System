8db798a983acb6d7b190c9900e3cab96da388504
Nush-Faiz <nushkiyafaiz15@gmail.com>
2025-04-21 21:01:15 +0530

    Updated settings.py with email configuration

--- movie_ticket_system/movie_ticket_system/settings.py
+++ movie_ticket_system/movie_ticket_system/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -128,3 +132,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 465
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
+    EMAIL_HOST_USER = os.getenv('bookflixfeedback@gmail.com')
+    EMAIL_HOST_PASSWORD = os.getenv('kacf goha aufy iqub')
+    DEFAULT_FROM_EMAIL = os.getenv('bookflixfeedback@gmail.com')
