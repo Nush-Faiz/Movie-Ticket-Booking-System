8d3d052a923a12d2e5ecf99f369be163eea60ffe
Piu Perera <piumininavodya08@gmail.com>
2025-05-05 11:56:09 +0530

    Modified book_ticket.html

--- movie_ticket_system/tickets/urls.py
+++ movie_ticket_system/tickets/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -10,4 +11,8 @@ urlpatterns = [
     path('about-us/', views.about_us, name='about_us'),
     path('faqs/', views.faqs, name='faqs'),
     path('feedback/', views.feedback, name='feedback'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('profile/', views.user_profile, name='profile'),
 ]
